315de7052b3cf835f1aa0a1f974ea050c3cce43c
Renato Liao <renatoliao06117@gmail.com>
2026-06-21 18:57:50 -0300

    Create login.php

---
+++ VIEW/login.php
@@ -0,0 +1,19 @@
+<?php
+    include_once $_SERVER['DOCUMENT_ROOT'] . "/Bibliotecaphp/DAL/usuario.php";
+    include_once $_SERVER['DOCUMENT_ROOT'] . "/Bibliotecaphp/MODEL/usuario.php";
+
+
+    $login = $_POST['login'];
+    $pwd = $_POST['pwd'];
+    $md5 = md5($pwd);
+
+    $dalUsuario = new \DAL\Usuario();
+    $usuario = $dalUsuario->SelectByLogin($login);
+
+    if($usuario != null && $md5 == $usuario->getSenha()){
+        session_start();
+        $_SESSION['login'] = $usuario;
+        header("location:Funcionario/listaFuncionario.php");
+    }
+    else header("location:index.php");
+?>
